b84e864e926d7f40074549842a6f242dd2956fe7
lucianbrn <lucianbrucker@gmail.com>
2026-05-04 20:54:57 -0400

    Add src/pages/_document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="Swipe-based hiring for full-time careers" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body><Main /><NextScript /></body>
+    </Html>
+  )
+}
